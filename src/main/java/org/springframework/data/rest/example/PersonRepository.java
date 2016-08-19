b9cf446b313bcf6a872da2e8402646e13b43aadd
root <root@localhost.localdomain>
2016-08-19 13:52:05 +0000

    update deps and versions of spring platforms

--- src/main/java/org/springframework/data/rest/example/PersonRepository.java
+++ src/main/java/org/springframework/data/rest/example/PersonRepository.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.repository.annotation.RestResource;
-
+//import org.springframework.data.rest.repository.annotation.RestResource;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 /**
  * @author Jon Brisbin <jon@jbrisbin.com>
  */
-@RestResource(path = "people")
+@RepositoryRestResource(path = "people")
+//@RestResource(path = "people")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
   @RestResource(path = "name", rel = "names")
