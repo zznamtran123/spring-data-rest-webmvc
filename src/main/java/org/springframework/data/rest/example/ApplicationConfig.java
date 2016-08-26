bc3fa3881bbdb831d635d319c85f2214af15365b
Trong Dinh <trongducdinh@gmail.com>
2016-08-26 01:14:21 +0000

    minor

--- src/main/java/org/springframework/data/rest/example/ApplicationConfig.java
+++ src/main/java/org/springframework/data/rest/example/ApplicationConfig.java
@@ -5,16 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ApplicationConfig extends SpringBootServletInitializer {
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationConfig.class, args);
-	}
-
 	@Autowired
 	private PersonRepository people;
 	@Autowired
